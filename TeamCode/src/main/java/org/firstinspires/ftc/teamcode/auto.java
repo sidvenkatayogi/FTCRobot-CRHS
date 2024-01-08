ce4311a4bb4e8bd37142842df1ae2ed8e1308e73
sidvenkatayogi <sidvenkatayogi@gmail.com>
2024-01-08 13:05:13 -0600

    First commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="test1")
+public class auto extends LinearOpMode{
+    DcMotor motor;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+    }
+}
